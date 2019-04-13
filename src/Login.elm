19b4edcc7ad88d59df199dfaa4803f9de053a65c
nikos <nikos@mugsoft.io>
2019-04-14 00:23:01 +0400

    initial file

---
+++ src/Login.elm
@@ -0,0 +1,21 @@
+module Login exposing (main)
+
+import Bootstrap exposing (CDN, Grid)
+import Html exposing (Html, div)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    ()
+
+
+view : Model -> Html Msg
+view model =
+    div [] []
+
+
+main =
+    3
